22f9f231b727ac9b23380c180553ac792aa0e595
Apphunter <apphunter.ah@gmail.com>
2017-11-11 17:15:31 +0000

    Added working path for me

--- ReadJson/src/JsonReader.java
+++ ReadJson/src/JsonReader.java
@@ -1,22 +1,14 @@
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Iterator;
 import java.util.Scanner;
-
-
-
-
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-// TEST COMMAND FOR GIT PUSH
 
 public class JsonReader {
 
@@ -24,122 +16,114 @@ public class JsonReader {
 
 		JsonObject theWholeFile = readFile();
 		if (theWholeFile != null) {
-		
-			//System.out.println(theWholeFile.toString());
-			//System.out.print(getChildren(theWholeFile));			
-						
+
+			// System.out.println(theWholeFile.toString());
+			// System.out.print(getChildren(theWholeFile));
+
 			JsonArray children = getChildren(theWholeFile);
-			JsonObject child0 =  (JsonObject) children.get(0);
-			//System.out.print(getKind(child1));
-			//System.out.println(getRight(child0).toString());
+			JsonObject child0 = (JsonObject) children.get(0);
+			// System.out.print(getKind(child1));
+			// System.out.println(getRight(child0).toString());
 			System.out.println(getName(getWhat(getRight(child0))).toString());
-			
-		
-			
-			
+
 		} else {
 			System.out.println("Could not read the file!");
 		}
 
-		
 	}
 
 	public JsonObject readFile() throws IOException {
-				
+
 		Scanner sc = new Scanner(System.in);
 		String filename = sc.nextLine();
-		
-		//String content = new String(Files.readAllBytes(Paths.get(filename)));
-		
+
+		// String content = new String(Files.readAllBytes(Paths.get(filename)));
+
 		String content = "";
-		
-		try
-	    {
-			content = new String(Files.readAllBytes(Paths.get("C:/Users/test/workspace/ReadJson/src/"+filename)));
-	    }
-	    catch (IOException e)
-	    {
-	        e.printStackTrace();
-	    }
-	
-		
-		
-		
-		
-		
+
+		try {
+			// Your path DONT DELETE IT
+			// content = new String(Files.readAllBytes(Paths.get("C:/Users/test/workspace/ReadJson/src/" + filename)));
+			
+			// My path DONT DELETE IT
+			content = new String(Files.readAllBytes(Paths.get("/home/gengar/workspace/eclipse-git/SSofProject/ReadJson/src/" + filename)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		JsonObject jo;
-        JsonElement je = new JsonParser().parse(content);
-        jo = je.getAsJsonObject();
-		
-		
-        sc.close();
+		JsonElement je = new JsonParser().parse(content);
+		jo = je.getAsJsonObject();
+
+		sc.close();
 		return jo;
-		
+
 	}
-	private  JsonArray getChildren(JsonObject j) {
+
+	private JsonArray getChildren(JsonObject j) {
 
 		JsonArray children = j.get("children").getAsJsonArray();
 
 		return children;
 	}
 
-	private  Object getLeft(JsonObject j) {
+	private Object getLeft(JsonObject j) {
 
 		JsonObject left = j.get("left").getAsJsonObject();
 
 		return left;
 	}
 
-	private  JsonObject getRight(JsonObject j) {
+	private JsonObject getRight(JsonObject j) {
 
 		JsonObject right = j.get("right").getAsJsonObject();
 
 		return right;
 	}
 
-	private  JsonObject getWhat(JsonObject j) {
+	private JsonObject getWhat(JsonObject j) {
 
 		JsonObject what = j.get("what").getAsJsonObject();
 
 		return what;
 	}
 
-	private  Object getOffset(JsonObject j) {
+	private Object getOffset(JsonObject j) {
 
 		JsonObject offset = j.get("offset").getAsJsonObject();
 
 		return offset;
 	}
 
-	private  Object getValueObject(JsonObject j) {
+	private Object getValueObject(JsonObject j) {
 
 		JsonObject offset = j.get("value").getAsJsonObject();
 
 		return offset;
 	}
 
-	private  String getValue(JsonObject j) {
+	private String getValue(JsonObject j) {
 
 		String value = j.get("value").getAsString();
 
 		return value;
 	}
 
-	private  String getOperator(JsonObject j) {
+	private String getOperator(JsonObject j) {
 
 		String operator = j.get("operator").getAsString();
 
 		return operator;
 	}
 
-	private  String getName(JsonObject j) {
+	private String getName(JsonObject j) {
 
 		String name = j.get("name").getAsString();
 
 		return name;
 	}
 
-	private  String getKind(JsonObject j) {
+	private String getKind(JsonObject j) {
 
 		String kind = j.get("kind").getAsString();
 
